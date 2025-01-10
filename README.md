22b266d9bc960a30163d1c3d473e63330cbbea5f
SirPatschiii <tofue2003@gmx.de>
2025-01-10 20:55:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpoolerRestart
+A program written in java which restarts the spooler service from windows. Aims to solve problems when printers aren't printing according to orders.
---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
+    id("org.javamodularity.moduleplugin") version "1.8.15"
+}
+
+group = "de.sirpatschiii"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-core:1.5.16")
+    implementation("ch.qos.logback:logback-classic:1.5.16")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("org.postgresql:postgresql:42.7.4")
+}
+
+application {
+    mainClass.set("de.sirpatschiii.Main")
+}
+
+javafx {
+    version = "21"
+    modules(
+        "javafx.base", "javafx.controls", "javafx.fxml", "javafx.graphics", "javafx.media", "javafx.swing", "javafx.web"
+    )
+}
+
+java {
+    modularity.inferModulePath.set(false)
+}
