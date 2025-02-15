46f328410f32ad6b5f77a030dc46fc684931e2d0
SirPatschiii <tofue2003@gmx.de>
2025-02-15 20:11:25 +0100

    Reworked the error message generation and moved it to a separate method

--- src/main/java/de/sirpatschiii/alerts/errorhandler/ErrorHandler.java
+++ src/main/java/de/sirpatschiii/alerts/errorhandler/ErrorHandler.java
@@ -10,6 +10,8 @@ import javafx.stage.StageStyle;
 import org.slf4j.Logger;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class ErrorHandler {
@@ -38,6 +40,7 @@ public class ErrorHandler {
             errorHandlerController = loader.getController();
         } catch (IOException e) {
             logger.error("An error occurred while switching a scene!", e);
+            System.exit(-1);
         }
     }
 
@@ -45,19 +48,59 @@ public class ErrorHandler {
         // Log the error
         logger.error(message, throwable);
 
+        // Show the error handler to the user
+        ErrorHandler errorHandler = new ErrorHandler();
+        errorHandler.setMessage(getUserMessage(message, throwable));
+        errorHandler.setStacktrace(getUserStacktrace(throwable));
+        errorHandler.showAndWait();
+    }
+
+    private static String getUserMessage(String message, Throwable throwable) {
+        String userMessage = "";
+        String errorClass = Arrays.stream(throwable.getClass().toString().split("\\.")).toList().getLast();
+        String errorCause = Arrays.stream(throwable.getCause().getClass().toString().split("\\.")).toList().getLast();
+
+        // If the no custom message is defined, use the error message
+        if (message.equals(throwable.getMessage())) {
+            String errorMessage = throwable.getMessage();
+            userMessage += errorClass + " - " + errorMessage;
+        } else {
+            userMessage += errorClass + " - " + message;
+        }
+
+        // Append cause if existent
+        if (throwable.getCause() != null) {
+            userMessage += " - caused by " + errorCause;
+        }
+
+        return userMessage;
+    }
+
+    private static String getUserStacktrace(Throwable throwable) {
+        ArrayList<Throwable> causes = new ArrayList<>();
+
+        // Get causes
+        causes.addLast(throwable.getCause());
+        while (causes.getLast().getCause() != null) {
+            causes.addLast(causes.getLast().getCause());
+        }
+
         // Build the stack trace string
         StringBuilder sb = new StringBuilder();
         sb.append(throwable.getClass().toString().replace("class ", "")).append(": ").append(throwable.getMessage()).append("\n");
         for (StackTraceElement stackTraceElement : throwable.getStackTrace()) {
             sb.append("    at ").append(stackTraceElement.toString()).append("\n");
         }
-        String stacktrace = sb.toString().strip();
 
-        // Throw the error to the user
-        ErrorHandler errorHandler = new ErrorHandler();
-        errorHandler.setMessage(message + " - " + throwable.getMessage());
-        errorHandler.setStacktrace(stacktrace);
-        errorHandler.showAndWait();
+        // Add stack trace for causes
+        for (Throwable thr : causes) {
+            sb.append("caused by ").append(thr.getClass().toString().replace("class ", "")).append(": ").append(thr.getMessage()).append("\n");
+            for (StackTraceElement stackTraceElement : thr.getStackTrace()) {
+                sb.append("    at ").append(stackTraceElement.toString()).append("\n");
+            }
+        }
+
+        return sb.toString().strip();
     }
 
     public void setMessage(String message) {
